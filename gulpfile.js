1f748e3a146158de7b97271b324e7e549be6cdb0
Xiaojuan Xu <xjxu@thoughtworks.com>
2017-10-13 11:53:53 +0800

    Output html report

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
+const gutil = require('gulp-util');
 
-gulp.task('test', function(){
+gulp.task('test', function () {
     require('./config/env')('test');
     runTest();
 });
 
-gulp.task('prod', function(){
+gulp.task('prod', function () {
     require('./config/env')('prod');
     runTest();
 });
@@ -15,6 +16,10 @@ function runTest() {
     return gulp
         .src(['test/*.js'], {read: false})
         .pipe(mocha({
-            reporter: 'spec'
-        }));
+            reporter: 'mochawesome',
+            reporterOptions: {
+                reportDir: 'customReportDir',
+                reportName: 'cutomReportName'
+            }
+        }))
 }
